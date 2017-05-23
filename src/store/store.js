206418d4343729b89e52443ddf0bb2030141efdf
Sidney Tang <sidney.tang@elationhealth.com>
2017-05-23 16:24:50 -0700

    Set up simplistic toggling of redux-logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createNavigationEnabledStore } from '@expo/ex-navigation';
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import logger from 'redux-logger'; // eslint-disable-line no-unused-vars
 import rootReducer from './rootReducer';
 
 
@@ -9,6 +9,10 @@ const createStoreWithNavigation = createNavigationEnabledStore({
   navigationStateKey: 'navigation',
 });
 
-const store = createStoreWithNavigation(rootReducer, applyMiddleware(logger));
+const middlewares = [];
+// Uncomment below line when debugging, if needed. Don't commit the uncommented version though.
+//middlewares.push(logger);
+
+const store = createStoreWithNavigation(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
